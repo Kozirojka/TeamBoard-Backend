bc4da0b4ea0f611cbdba66c01e68a7e6bafb7fc3
Stalker <vihovaneckostik@gmil.com>
2025-03-16 17:23:58 +0200

    create some models an api project

---
+++ RealTimeBoardSol/RealTimeBoard.Domain/VectorObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+using System.Text.Json.Nodes;
+
+namespace RealTimeBoard.Domain;
+
+public class VectorObject
+{
+    public Guid Id { get; set; }
+    public FigureType Type { get; set; }
+    public Point Position { get; set; }
+    public string Color { get; set; }
+    public JsonNode? Data { get; set; }
+}
